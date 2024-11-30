daaf7aade6633b084b7cd753714d2d65a3c947f0
AngeloNorelli <angnorelli@gmail.com>
2024-11-30 16:23:12 +0100

    Dodanie CORSMiddleware

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.controllers import weather, summary
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://weather-energy-frontend.onrender.com/"],
+    allow_credentioals=True,
+    allow_methods=["GET"],
+    allow_headers=["*"]
+)
 
 app.include_router(weather.router)
 app.include_router(summary.router)
